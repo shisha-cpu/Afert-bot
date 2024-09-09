26e913bf23b21dbb7b79655e757edffa380f4d90
shisha-cpu <79801541+shisha-cpu@users.noreply.github.com>
2024-09-09 10:24:26 +0300

    end

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const TelegramBot = require('node-telegram-bot-api');
 const token = '7528483883:AAHmaABfsF6E29iTuyUOR5WGV95ltmQl-OE';
 const bot = new TelegramBot(token, {polling: true});
 
+// const adminChatId = '1137493485';
+const adminChatId = '197876343'; 
+
 bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
 
@@ -15,18 +18,43 @@ bot.onText(/\/start/, (msg) => {
           { text: 'Пожарная и техника безопасности', url: 'https://disk.yandex.ru/d/bTxBqtCo-Cqfig' }
         ],
         [
-          { text: 'Ознакомлен ✅', callback_data: 'acknowledged' }
+          { text: 'Договор услуги', url: 'https://docs.yandex.ru/docs/view?url=ya-disk-public%3A%2F%2F69nw1otFLpxsqdnPUR1lSzax2VTa6NhQwgYcb2wNV3kTfXwXUZ7phoRcElJFM1I0q%2FJ6bpmRyOJonT3VoXnDag%3D%3D%3A%2FДоговор%20субаренды.docx&name=Договор%20субаренды.docx' }
+        ],
+        [
+          { text: 'Договор субаренды', url: 'https://docs.yandex.ru/docs/view?url=ya-disk-public%3A%2F%2F69nw1otFLpxsqdnPUR1lSzax2VTa6NhQwgYcb2wNV3kTfXwXUZ7phoRcElJFM1I0q%2FJ6bpmRyOJonT3VoXnDag%3D%3D%3A%2FДоговор%20услуги%20.docx&name=Договор%20услуги%20.docx' }
+        ],
+        [
+          { text: 'Оферта и Политика данных ознакомлен ✅', callback_data: 'offer_acknowledged' }
+        ],
+        [
+          { text: 'Услуги ознакомлен ✅', callback_data: 'service_acknowledged' }
+        ],
+        [
+          { text: 'Субаренда ознакомлен ✅', callback_data: 'sublease_acknowledged' }
         ]
       ]
     }
   };
 
-  bot.sendMessage(chatId, 'Пожалуйста, ознакомьтесь с документами по ссылкам ниже:', options);
+  bot.sendMessage(chatId, 'Пожалуйста, ознакомьтесь с документами по ссылкам ниже и подтвердите ознакомление:', options);
 });
 
 bot.on('callback_query', (query) => {
   const chatId = query.message.chat.id;
-  if (query.data === 'acknowledged') {
-    bot.sendMessage(chatId, 'Ваше согласие получено ✅');
+  const username = query.from.username || 'пользователь';
+
+  if (query.data === 'offer_acknowledged') {
+    bot.sendMessage(chatId, 'Ваше согласие с Офертой и Политикой данных получено ✅');
+    bot.sendMessage(adminChatId, `Пользователь @${username} (ID: ${chatId}) согласен с Офертой и Политикой данных.`);
+  }
+
+  if (query.data === 'service_acknowledged') {
+    bot.sendMessage(chatId, 'Ваше согласие с Договором услуг получено ✅');
+    bot.sendMessage(adminChatId, `Пользователь @${username} (ID: ${chatId}) согласен с Договором услуг.`);
+  }
+
+  if (query.data === 'sublease_acknowledged') {
+    bot.sendMessage(chatId, 'Ваше согласие с Договором субаренды получено ✅');
+    bot.sendMessage(adminChatId, `Пользователь @${username} (ID: ${chatId}) согласен с Договором субаренды.`);
   }
 });
